ec6efa8acbcdbbdfead006f99b856a2c18521df1
吕海涛 <git@lvht.net>
2015-12-10 18:32:21 +0800

    启用日志

--- php/phpcd_main.php
+++ php/phpcd_main.php
@@ -1,4 +1,5 @@
 <?php
+require __DIR__ . '/init_log.php';
 require $argv[2];
 require __DIR__ . '/PHPCD.php';
 (new PHPCD($argv[1]))->loop();
--- php/phpid_main.php
+++ php/phpid_main.php
@@ -1,4 +1,5 @@
 <?php
+require __DIR__ . '/init_log.php';
 require $argv[2];
 require __DIR__ . '/PHPID.php';
 (new PHPID($argv[1], $argv[3], $argv[4]))->loop();
