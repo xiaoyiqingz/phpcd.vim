8b033a57c514b2d13831c703e7dfa91d85f3bc8e
吕海涛 <git@lvht.net>
2015-12-19 12:22:11 +0800

    vendor/autoload.php should only for PHPCD development

--- php/phpcd_main.php
+++ php/phpcd_main.php
@@ -1,7 +1,10 @@
 <?php
 error_reporting(0);
 
-require dirname(__DIR__). '/vendor/autoload.php';
 require $argv[1] . '/vendor/autoload.php';
+
+require __DIR__ . '/RpcServer.php';
+require __DIR__ . '/PHPCD.php';
+require __DIR__ . '/Reflection/ReflectionClass.php';
 
 (new PHPCD)->loop();
