9bd5c5486ec03343a3e0c50e36d91cd86d41a46d
przepompownia <przepompownia@users.noreply.github.com>
2015-12-19 04:41:44 +0100

    Remove redundant class loading

--- php/phpcd_main.php
+++ php/phpcd_main.php
@@ -3,7 +3,5 @@ error_reporting(0);
 
 require dirname(__DIR__). '/vendor/autoload.php';
 require $argv[1] . '/vendor/autoload.php';
-require __DIR__ . '/RpcServer.php';
-require __DIR__ . '/PHPCD.php';
 
 (new PHPCD)->loop();
